56817880e7505698d92f49b7519f7923256c9e71
Daniel Hilgert <ginferdaniel@gmail.com>
2023-05-29 12:46:53 -0300

    javascript

---
+++ Desktop/2dapre/ejemplos.js
@@ -0,0 +1,36 @@
+//reduce para los prad del carrito
+// productos.reduce(()=>{
+//    
+//},0)
+
+//Busqueda de productos
+//Array de objetos
+//calcular el costo del envio
+//function calcularEnvio(){
+//    return total +900
+//}
+//
+// if (){
+// }else{   
+// }
+
+// let totalCompra = carrito.reduce((accum, prod) => {
+//     return accum + prod.precio}, 0)
+
+//     console.log(totalCompra)
+//
+// 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
